80eb8d2a8ce94c0fabb37dbe78fd9692a1d04566
yashvir24 <76519857+yashvir24@users.noreply.github.com>
2021-01-13 17:03:07 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,17 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            restitution:1.0
+        }
+        this.body= Bodies.rectangle(x,y,width,height,options);
+
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body)
+    }
+    display() {
+       rectMode(CENTER);
+       fill(255);
+       rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    }
+}
